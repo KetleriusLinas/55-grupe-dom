bad66a74c14be29c7f9000dca17eb29baae99b0b
Linas <ketleriuslinas@gmail.com>
2025-05-20 10:29:32 +0300

    add-block/ add-todo

--- js/cards.js
+++ js/cards.js
@@ -1,49 +1,46 @@
 const listDOM = document.querySelector('.list');
 
-// const exampleHTML = `
-//             <div class="card">
-//                 <img class="card-img" src="#" alt="">
-//                 <h2 class="card-title">Pavadinimas</h2>
-//                 <p class="card-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni veritatis corporis cupiditate aspernatur animi aliquid voluptate, rerum minus id facere facilis autem iste obcaecati. Repellat numquam veniam odio quos asperiores.</p>
-//                 <a class="card-link" href="#">Read more</a>
-//             </div>`;
-
 const data = [
     {
         imgSrc: 'car_1.jpg',
-        imgAlt: 'car 1',
+        imgAlt: 'Car 1',
         title: 'Car 1 title',
-        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni veritatis corporis cupiditate aspernatur animi aliquid voluptate, rerum minus id facere facilis autem iste obcaecati. Repellat numquam veniam odio quos asperiores',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem provident quaerat necessitatibus quasi veritatis dicta consectetur debitis aliquam nobis consequuntur.',
         link: '#',
     },
     {
         imgSrc: 'car_2.jpg',
-        imgAlt: 'car 2',
+        imgAlt: 'Car 2',
         title: 'Car 2 title',
-        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni veritatis corporis cupiditate aspernatur animi aliquid voluptate, rerum minus id facere facilis autem iste obcaecati. Repellat numquam veniam odio quos asperiores',
+        desc: 'Lorem ipsum dolor sit amet provident quaerat necessitatibus quasi veritatis dicta consectetur debitis aliquam nobis consequuntur.',
         link: '#',
     },
     {
         imgSrc: 'car_3.jpg',
-        imgAlt: 'car 3',
+        imgAlt: 'Car 3',
         title: 'Car 3 title',
-        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni veritatis corporis cupiditate aspernatur animi aliquid voluptate, rerum minus id facere facilis autem iste obcaecati. Repellat numquam veniam odio quos asperiores',
+        desc: 'Lorem ipsum dolor sit amet provident quaerat necessitatibus quasi veritatis dicta consectetur debitis aliquam nobis consequuntur.',
+        link: '#',
+    },
+    {
+        imgSrc: 'car_4.jpg',
+        imgAlt: 'Car 4',
+        title: 'Car 4 title',
+        desc: 'Lorem ipsum dolor sit amet provident quaerat necessitatibus quasi veritatis dicta consectetur debitis aliquam nobis consequuntur.',
         link: '#',
     },
 ];
 
 let HTML = '';
 
-// for (let i = 0; i < 10; i++)
 for (const item of data) {
-    // listDOM.innerHTML +=
     HTML += `
-            <div class="card">
-                <img class="card-img" src="../img/${item.imgSrc}" alt="${item.imgAlt}">
-                <h2 class="card-title">${item.title}</h2>
-                <p class="card-desc">${item.desc}</p>
-                <a class="card-link" href="${item.link}">Read more</a>
-            </div>`;
+        <div class="card">
+            <img class="card-img" src="../img/${item.imgSrc}" alt="${item.imgAlt}">
+            <h2 class="card-title">${item.title}</h2>
+            <p class="card-desc">${item.desc}</p>
+            <a class="card-link" href="${item.link}">Read more...</a>
+        </div>`;
 }
 
 listDOM.innerHTML = HTML;
